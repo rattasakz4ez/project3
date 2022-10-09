f62bed1c5445609f5ca9b6d91a0138b1fec3f0f8
somyod2010 <somyod_2010@hotmail.com>
2022-10-09 10:44:53 +0700

    first commit

---
+++ navbar.php
@@ -0,0 +1,13 @@
+<ul>
+    <li><a href="index.php"> Home </a></li>
+    <li><a href="about.php"> เกี่ยวกับเรา </a></li>
+    <li><a href="contact.php"> ติดต่อเรา </a></li>
+    <li><a href="product.php"> สินค้า </a></li>
+    <li><a href="blog_form.php">blogs</a></li>
+    <li><a href="cat_foem.php">ประเภทสินค้า</a></li>
+    <li><a href="blog forg.php">สินค้า</a></li>
+
+    <li style="float: right;"><a href="resume.php"> ผู้จัดทำ </a></li>
+    <li style="float: right;"><a href="login.php"> Login </a></li>
+    <li style="float: right;"><a href="register.php"> Register </a></li>
+</ul>
---
+++ product_form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    mit" <form action="product_insert.php" method="post" enctype="multipart/form-data"><br />
+        name <input type="text" name="name" /><br />
+        price <input type="text" name="price" /> <br />
+        stock<input type="text" name="stock" /> <br />
+        cat_name<input type="text" name="cat_name" /> <br />
+        description<textarea rows=" 5" cols="50" name="description"> </textarea><br />
+        แทรกรูป <input type="file" name="img" /> <br />
+        <input type="subvalue="บัณทึก" /><br />
+    </form>
+
+</body>
+
+</html>
